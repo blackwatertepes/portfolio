38a932e6d6f5d905bad19fe1f677da6dac209d8a
Tyler J. Kuhn <tyler@wargoats.com>
2024-06-01 11:43:38 -0700

    Update About.js

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ function About() {
     <React.Fragment>
       <Typography variant="h6">Profile</Typography>
       <Typography variant="body1">
-        I'm a tech startup veteran, with more than 14 years of industry experience.
+        I'm a tech startup veteran, with more than 18 years of industry experience.
         I've worn multiple hats, starting in Flash/ActionScript 2.0, and eventually transitioning into Full-Stack web development.
         I've dabbled in a variety of web technologies, including Ruby/Rails, NodeJS, PostgresQL, React, Docker, Heroku, & various bits of AWS.
         When I'm not programming, you can usually find me riding my electric skateboard around San Francisco.
